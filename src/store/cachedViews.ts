9aeab87c0b8fb6adc85ca58671c3f115541f770b
naijiang_he <1134163212@qq.com>
2021-04-26 17:49:00 +0800

    框架搭建

--- src/store/cachedViews.ts
+++ src/store/cachedViews.ts
@@ -3,14 +3,24 @@ import { Commit } from "vuex";
 /**需要缓存的页面 */
 export default {
   state: {
-    cachedViews: [],
+    cachedViews:
+      JSON.parse(localStorage.getItem("cachedViews") as string) || [],
     cachedViewsName: []
   },
   mutations: {
     /**添加tabs标签 */
     addTabsViews(state: { cachedViews: Route[] }, view: Route) {
       if (!state.cachedViews.find(element => element.name == view.name)) {
-        state.cachedViews.push(view);
+        let obj: any = {
+          path: view.path,
+          name: view.name,
+          meta: view.meta,
+          query: view.query,
+          params: view.params,
+          fullPath: view.fullPath
+        };
+        state.cachedViews.push(obj);
+        localStorage.setItem("cachedViews", JSON.stringify(state.cachedViews));
       }
     },
     /**移除tabs标签 */
@@ -19,6 +29,7 @@ export default {
         element => element.name == view.name
       );
       state.cachedViews.splice(index, 1);
+      localStorage.setItem("cachedViews", JSON.stringify(state.cachedViews));
     },
     /**添加缓存组件名称 */
     addCachedViewsName(
@@ -34,7 +45,7 @@ export default {
     }
   },
   getters: {
-    getTabsViews(state: { cachedViews: RouteRecord[] }) {
+    getTabsViews(state: { cachedViews: Route[] }) {
       return state.cachedViews;
     },
     getCachedViewsNane(state: { cachedViewsName: string[] }) {
